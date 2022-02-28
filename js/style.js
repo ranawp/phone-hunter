b0b339d0773d3808e75f25c7cd8e8eed98efb623
ranawp <ranasaha4224#gmail.com>
2022-03-01 01:29:33 +0600

    single mobile details added

--- js/style.js
+++ js/style.js
@@ -1,34 +1,101 @@
+//get input field data
 const getMobileData = () => {
     const inputField = document.getElementById('input-field').value
     inputField.value = ''
     loadData(inputField)
-
 }
-const loadData = async (inputField) => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=${inputField}`
+
+// fetch data from api
+const loadData = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     const res = await fetch(url)
     const data = await res.json()
     displayData(data.data)
 }
 
+//display all data
 const displayData = mobileData => {
     const displayMobile = document.getElementById('display-mobile')
+    displayMobile.textContent = ''
     mobileData.forEach(data => {
         const div = document.createElement('div')
         div.classList.add('col-lg-4')
         div.classList.add('col-12')
         div.innerHTML = `
         
-        <div class="card" style="width: 18rem;">
+        <div class="card mb-3" style="width: 18rem;">
   <img width="img-fluid"  src="${data.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${data.phone_name}</h5>
     <p class="card-text">${data.brand}</p>
-    <a onclick='singlePhoneDetail(${data.id})' href="#" class="btn btn-primary">Details</a>
+    <a onclick='singlePhoneDetail("${data.slug}")' href="#" class="btn btn-primary">Details</a> 
   </div>
 </div>`
         displayMobile.appendChild(div)
 
     });
 }
+
+// displayData('iPhone 13 mini')
+
+//fetch mobile details 
+const singlePhoneDetail = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    const res = await fetch(url)
+    const data = await res.json()
+    displaySinglephone(data.data)
+
+}
+
+const displaySinglephone = data => {
+    // console.log(data)
+    const singleMobile = document.getElementById('single-Mobile')
+    singleMobile.textContent = ''
+    const div = document.createElement('div')
+    div.innerHTML = ` 
+        <div class="card" style="width: 18rem;">
+  <img src="${data.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+
+    <h5 class="card-title"><span class="font-weight-bold text-info
+
+    ">Name:</span>${data.name}</h5>
+    <p class="card-title"><span class="text-info
+
+    font-weight-bold">Brand:</span>${data.brand}</p>
+    <p class="card-text text-success"><span class="text-info font-weight-bold">Relase Date: </span>${data.releaseDate}</p>
+    <p class="text-success card-text"><span class="text-info font-weight-bold">Cheap set:</span>${data.chipSet}</p>
+    <p class="text-success card-text font-weight-bold"><span class="text-info font-weight-bold">Memory:</span>${data.memory}</p>
+    <p class="text-success card-text"><span class="text-info font-weight-bold">Storage:</span>${data.storage}</p>
+    <p class="text-success card-text"><span class="text-info font-weight-bold">Display Size:</span>${data.displaySize}</p>
+  </div>
+</div>
+        `
+    singleMobile.appendChild(div)
+
+
+    /*  data.forEach(mobileDetails => {
+         console.log(mobileDetails)
+ 
+         div.innerHTML = ` 
+         <div class="card" style="width: 18rem;">
+   <img src="${mobileDetails.image}" class="card-img-top" alt="...">
+   <div class="card-body">
+ 
+     <h5 class="card-title">Brand:${mobileDetails.name}</h5>
+     <p class="card-title">Brand:${mobileDetails.brand}</p>
+     <p class="card-text">Relase Date: ${mobileDetails.releaseDate}</p>
+     <p class="card-text">Cheap set:${mobileDetails.chipSet}</p>
+     <p class="card-text">Memory:${mobileDetails.memory}</p>
+     <p class="card-text">Storage:${mobileDetails.storage}</p>
+     <p class="card-text">Display Size:${mobileDetails.displaySize}</p>
+   </div>
+ </div>
+         `
+         singleMobile.appendChild(div)
+ 
+     }) */
+}
+
