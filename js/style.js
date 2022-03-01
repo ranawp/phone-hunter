0d86dd2e8376d6f0dd0f412a353b0cd3f2c01305
ranawp <ranasaha4224#gmail.com>
2022-03-01 16:06:42 +0600

    single  censor data

--- js/style.js
+++ js/style.js
@@ -8,14 +8,14 @@ const toggoleSearch = displayStyle => {
     document.getElementById('all-mobile').style.display = displayStyle
 }
 //error massage 
-const errorMassage = displayStyle => {
+/* const errorMassage = displayStyle => {
     document.getElementById('error-massage').style.display = displayStyle
 }
-
+ */
 //get input field data
 const getMobileData = () => {
     const inputField = document.getElementById('input-field').value
-    inputField.value = ''
+    document.getElementById('input-field').value = ''
     toggleSpinner('block')
     toggoleSearch('none')
     loadData(inputField)
@@ -30,17 +30,18 @@ const loadData = async (searchText) => {
     displayData(data.data)
 }
 
-//display all data
+
+//display all mobile data
 const displayData = mobileData => {
     const displayMobile = document.getElementById('display-mobile')
+    const sliceData = mobileData.slice(0, 20)
     displayMobile.textContent = ''
-    if (mobileData) {
-        mobileData.forEach(data => {
-            const div = document.createElement('div')
-            div.classList.add('col-lg-4')
-            div.classList.add('col-12')
-            div.innerHTML = `
-            
+
+    sliceData.forEach(data => {
+        const div = document.createElement('div')
+        div.classList.add('col-lg-4')
+        div.classList.add('col-12')
+        div.innerHTML = `
             <div class="card mb-3" style="width: 18rem;">
       <img width="img-fluid"  src="${data.image}" class="card-img-top" alt="...">
       <div class="card-body">
@@ -48,15 +49,12 @@ const displayData = mobileData => {
         <p class="card-text">${data.brand}</p>
         <a onclick='singlePhoneDetail("${data.slug}")' href="#" class="btn btn-primary">Details</a> 
       </div>
-    </div>`
-            displayMobile.appendChild(div)
-
-        });
-        errorMassage('none')
-    } else {
-        errorMassage('block')
-    }
-
+    </div>
+    
+    `
+        displayMobile.appendChild(div)
+    });
+    //spinner display
     toggleSpinner('none')
     toggoleSearch('block')
 }
@@ -73,9 +71,9 @@ const singlePhoneDetail = async (id) => {
 }
 
 const displaySinglephone = data => {
-    // console.log(data)
+    console.log(data.mainFeatures.displaySize)
     const singleMobile = document.getElementById('single-Mobile')
-    singleMobile.textContent = ''
+    singleMobile.innerHTML = ''
     const div = document.createElement('div')
     div.innerHTML = ` 
         <div class="card" style="width: 18rem;">
@@ -89,36 +87,19 @@ const displaySinglephone = data => {
 
     font-weight-bold">Brand:</span>${data.brand}</p>
     <p class="card-text text-success"><span class="text-info font-weight-bold">Relase Date: </span>${data.releaseDate}</p>
-    <p class="text-success card-text"><span class="text-info font-weight-bold">Cheap set:</span>${data.chipSet}</p>
-    <p class="text-success card-text font-weight-bold"><span class="text-info font-weight-bold">Memory:</span>${data.memory}</p>
-    <p class="text-success card-text"><span class="text-info font-weight-bold">Storage:</span>${data.storage}</p>
-    <p class="text-success card-text"><span class="text-info font-weight-bold">Display Size:</span>${data.displaySize}</p>
+    <p class="text-success card-text"><span class="text-info font-weight-bold">Cheap set:</span>${data.mainFeatures.chipSet}</p>
+    <p class="text-success card-text font-weight-bold"><span class="text-info font-weight-bold">Memory:</span>${data.mainFeatures.memory}</p>
+    <p class="text-success card-text"><span class="text-info font-weight-bold">Storage:</span>${data.mainFeatures.storage}</p>
+    <p class="text-success card-text"><span class="text-info font-weight-bold">Display Size:</span>${data.mainFeatures.displaySize}</p>
+
+   
+    <p class="text-success card-text"><span class="text-info font-weight-bold">Censor:</span>${data.mainFeatures.sensors[0]}
+    ${data.mainFeatures.sensors[1]},
+    ${data.mainFeatures.sensors[2]}, ${data.mainFeatures.sensors[3]}${data.mainFeatures.sensors[4]}</p>
+    
   </div>
 </div>
         `
     singleMobile.appendChild(div)
-
-
-    /*  data.forEach(mobileDetails => {
-         console.log(mobileDetails)
- 
-         div.innerHTML = ` 
-         <div class="card" style="width: 18rem;">
-   <img src="${mobileDetails.image}" class="card-img-top" alt="...">
-   <div class="card-body">
- 
-     <h5 class="card-title">Brand:${mobileDetails.name}</h5>
-     <p class="card-title">Brand:${mobileDetails.brand}</p>
-     <p class="card-text">Relase Date: ${mobileDetails.releaseDate}</p>
-     <p class="card-text">Cheap set:${mobileDetails.chipSet}</p>
-     <p class="card-text">Memory:${mobileDetails.memory}</p>
-     <p class="card-text">Storage:${mobileDetails.storage}</p>
-     <p class="card-text">Display Size:${mobileDetails.displaySize}</p>
-   </div>
- </div>
-         `
-         singleMobile.appendChild(div)
- 
-     }) */
 }
 
